c0e62c4c9c197c9da2ca74dff4e5766c4c5cff86
KiwookJUNG <ismeeki17@naver.com>
2019-11-20 02:19:07 +0900

    MemoList,MemoCompose,MemoDetail ViewController 에 각각 ViewModelBindableType 프로토콜을 채택하고 viewModel 프로퍼티에 타입 할당과 bindViewModel 메소드 추가.

--- RxMemo/ViewController/MemoComposeViewController.swift
+++ RxMemo/ViewController/MemoComposeViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class MemoComposeViewController: UIViewController {
+class MemoComposeViewController: UIViewController, ViewModelBindableType {
 
+    var viewModel: MemoComposeViewModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,14 +19,8 @@ class MemoComposeViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  func bindViewModel() {
+           
+ }
 
 }
--- RxMemo/ViewController/MemoDetailViewController.swift
+++ RxMemo/ViewController/MemoDetailViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class MemoDetailViewController: UIViewController {
+class MemoDetailViewController: UIViewController, ViewModelBindableType {
 
+    var viewModel : MemoDetailViewModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,14 +19,8 @@ class MemoDetailViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func bindViewModel() {
+        
     }
-    */
 
 }
