3f40614750e741b540d9ed6b1d5b1e550d977371
KiwookJUNG <ismeeki17@naver.com>
2019-11-20 02:00:39 +0900

    MemoListViewController MemoDetailViewController MemoComposeViewController 뷰컨트롤러 파일 생성 및 스토리보드의 신과 연결

---
+++ RxMemo/ViewController/MemoComposeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MemoComposeViewController.swift
+//  RxMemo
+//
+//  Created by 정기욱 on 2019/11/20.
+//  Copyright © 2019 kiwook. All rights reserved.
+//
+
+import UIKit
+
+class MemoComposeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ RxMemo/ViewController/MemoListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MemoListViewController.swift
+//  RxMemo
+//
+//  Created by 정기욱 on 2019/11/20.
+//  Copyright © 2019 kiwook. All rights reserved.
+//
+
+import UIKit
+
+class MemoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
