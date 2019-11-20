200ef100e92fe9eea4ae44bfd8cc5692df84dc97
KiwookJUNG <ismeeki17@naver.com>
2019-11-20 18:00:37 +0900

    MemoListViewController UI 구성

--- RxMemo/ViewController/MemoListViewController.swift
+++ RxMemo/ViewController/MemoListViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
+
 class MemoListViewController: UIViewController, ViewModelBindableType {
     
     var viewModel : MemoListViewModel!
     
+    @IBOutlet weak var listTableView: UITableView!
 
+    @IBOutlet weak var addButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
