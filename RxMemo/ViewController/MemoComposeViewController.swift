97b16e7630b39435345d788fc7947b1b2ae23ddd
KiwookJUNG <ismeeki17@naver.com>
2019-11-20 21:39:56 +0900

    1. MemoComposeViewController의 스토리보드 신에 두개의 바 버튼과 텍스트 필드를 추가
    2. 화면에 진입할때 textField를 firstResponder로 화면에서 벗어날때 resign
    
    3. 뷰 모델에 정의한 네이게이션 타이틀과 이니셜 텍스트를 드라이버로 바인딩
    
    4. 뷰의 캔슬버튼을 뷰 모델의 캔슬 액션과 바인딩
    
    5. 세이브 버튼을 바인딩

--- RxMemo/ViewController/MemoComposeViewController.swift
+++ RxMemo/ViewController/MemoComposeViewController.swift
@@ -7,9 +7,17 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import Action
+import NSObject_Rx
 
 class MemoComposeViewController: UIViewController, ViewModelBindableType {
 
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var contentTextView: UITextView!
+    
     var viewModel: MemoComposeViewModel!
     
     override func viewDidLoad() {
@@ -18,9 +26,37 @@ class MemoComposeViewController: UIViewController, ViewModelBindableType {
         // Do any additional setup after loading the view.
     }
     
-
-  func bindViewModel() {
-           
- }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        contentTextView.becomeFirstResponder()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if contentTextView.isFirstResponder {
+            contentTextView.resignFirstResponder()
+        }
+    }
+    
+    
+    // 뷰 모델을 뷰에 바인딩
+    func bindViewModel() {
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: rx.disposeBag)
+        
+        viewModel.initialText
+            .drive(contentTextView.rx.text)
+            .disposed(by: rx.disposeBag)
+        
+        cancelButton.rx.action = viewModel.cancelAction
+        
+        saveButton.rx.tap
+            .throttle(.milliseconds(500), scheduler: MainScheduler.instance)
+            .withLatestFrom(contentTextView.rx.text.orEmpty)
+            .bind(to: viewModel.saveAction.inputs)
+            .disposed(by: rx.disposeBag)
+    }
 
 }
