5f8e2c812c959c3bd698abb38bf1cc4bb0089425
KiwookJUNG <ismeeki17@naver.com>
2019-12-25 23:19:59 +0900

    1. MemoDetailViewModel 구현 :  MemoModel을 사용한 contents를 가지고 있는 옵저버블(BehaviorSubject)을 생성한다. 의존성을 주입한다.
    2. MemoDetailViewController 의 뷰를 뷰모델을 이용해 모델과 바인딩한다.

--- RxMemo/ViewController/MemoDetailViewController.swift
+++ RxMemo/ViewController/MemoDetailViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import NSObject_Rx
 
 class MemoDetailViewController: UIViewController, ViewModelBindableType {
 
@@ -28,7 +31,30 @@ class MemoDetailViewController: UIViewController, ViewModelBindableType {
     
 
     func bindViewModel() {
+        viewModel.title
+            .drive(navigationItem.rx.title)
+            .disposed(by: rx.disposeBag)
         
-    }
+        viewModel.contents
+            .bind(to: listTableView.rx.items) { tableView, row, value in
+                switch row {
+                case 0:
+                    let cell = tableView.dequeueReusableCell(withIdentifier: "contentCell")!
+                    cell.textLabel?.text = value
+                    return cell
+                    
+                case 1:
+                    let cell = tableView.dequeueReusableCell(withIdentifier: "dateCell")!
+                    cell.textLabel?.text = value
+                    return cell
+                    
+                default:
+                    fatalError()
+                }
+            }
+            .disposed(by: rx.disposeBag)
+                
+        }
+    
 
 }
